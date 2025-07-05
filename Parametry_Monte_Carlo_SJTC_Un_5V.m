e217b55d18182c5b884db7d287d05758b2340a7e
Michal15kV <Wrona747@wp.pl>
2025-07-05 05:56:00 +0200

    Add files via upload

---
+++ Parametry_Monte_Carlo_SJTC_Un_5V.m
@@ -0,0 +1,199 @@
+Z = 50;
+lenCu1 = 9e-3;
+lenCu2 = 4e-3;
+lenCu3 = 3e-3;
+radiusCu1p = 1.5e-3;
+radiusCu = 1e-3;
+radiusCu3Pin = 1.15e-3;
+radiusNpin = 1.52e-3;
+conNtube = 9.3e6;
+thickTubeSectionAir = 4e-3;
+conNinternal = 9.3e6;
+lenSectionAir = 18e-3;
+lenSectionTefM = 7e-3;
+lenSectionTefF = 17e-3;
+thickDisk = 10e-3;
+conDisk = 15.08e6;
+conTube = 4.6e7;
+lenSjtcL = 8.7e-3;
+lenSjtcH = 6.6e-3;
+radiusTube = 30e-3;
+thickTube = 2e-3;
+conConWires = 5.7e7;
+lenRangeRes = 132e-3;
+conRangeRes = 0.77e6;
+radiusNtubeAir = 3.5e-3;
+radiusNtubeOuter = 8.8e-3;
+miTeflon = 1;
+epsylonTeflon = 2.1; 
+miAir = 1;
+epsylonAir = 1;
+miCopper = 1;
+radiusRangeRes = 7.5e-6;
+zLoad = 90;
+radiusSjtcL = 0.252e-3;
+conSJTCHeater = 0.752e6;
+lenCu1p = 6e-3;
+miHeater = 1;
+miTube = 1;
+miDisk = 1;
+miRangeRes = 1; 
+miNinternal = 1;
+miNtube = 1;
+conCu1p = 28e6;
+radiusNtubeTef = 5.1e-3;
+thickTubeSectionTeflon = 3.7e-3;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+epsylonTeflon_delta = 0.05;
+conNtube_delta = 2.3e6;
+conNinternal_delta = 2.3e6;
+conDisk_delta = 0.05e6;
+conTube_delta = 0.4e6;
+conCu1p_delta = 2.9e6;
+conConWires_delta = 3e6;
+conRangeRes_delta = 0.06e6;
+conSJTCHeater_delta = 0.003e6;
+thickTubeSectionAir_delta = 1e-3;
+thickDisk_delta = 0.1e-3;
+thickTube_delta = 0.3e-3;
+lenSectionAir_delta = 1e-3;
+lenSectionTefM_delta = 1e-3;
+lenSectionTefF_delta = 1e-3;
+lenCu1_delta = 1e-3;
+lenCu1p_delta = 0.1e-3;
+lenRangeRes_delta = 1e-3;
+lenCu2_delta = 1e-3;
+lenSjtcL_delta = 1e-3;
+lenSjtcH_delta = 1e-3;
+lenCu3_delta = 1e-3;
+radiusNtubeOuter_delta = 0.3e-3;
+radiusNpin_delta = 0.01e-3;
+radiusNtubeAir_delta = 0.01e-3;
+radiusTube_delta = 0.1e-3;
+radiusCu_delta = 0.05e-3;
+radiusCu1p_delta = 0.05e-3;
+radiusRangeRes_delta = 0.4e-6;
+radiusSjtcL_delta = 0.5e-6;
+radiusCu3Pin_delta = 0.05e-3;
+zLoad_delta = 10;
+radiusNtubeTef_delta = 4.7e-6;
+thickTubeSectionTeflon_delta = 1.8e-4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+%}
+
+
+
+
+
+
+
+
+
+
+
+
